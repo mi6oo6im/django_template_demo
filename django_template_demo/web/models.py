74c2718d7c2ad64873ee5d94f080bf8838b849e5
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-06-14 21:50:23 +0100

    add models

--- django_template_demo/web/models.py
+++ django_template_demo/web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.template.defaultfilters import first
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=40, null=False, blank=False)
+    last_name = models.CharField(max_length=40, null=False, blank=False)
+    middle_name = models.CharField(max_length=10, null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    email_address = models.EmailField()
